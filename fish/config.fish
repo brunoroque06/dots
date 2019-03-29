0175598cfbee27e7f73901c36f99ee920413757e
Bruno Roque <bruno.roque@spoud.io>
2019-03-29 18:34:55 +0100

    Add fish folder

---
+++ fish/config.fish
@@ -0,0 +1,4 @@
+# Colors
+set -U fish_color_command green
+set -U fish_color_param normal
+set -U fish_color_quote yellow
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,38 @@
+function prompt::dir
+  set_color -b blue black
+  printf ' %s ' (pwd | sed "s,^$HOME,~,")
+end
+
+function prompt::git
+  set -l branch (git rev-parse --abbrev-ref HEAD)
+  if test (git status -s) -z
+    set_color -b green black
+  else
+    set_color -b yellow black
+  end
+  printf ' ⎇ %s ' "$branch"
+end
+
+function prompt::status
+  set_color -b normal red
+  if [ $argv[1] -ne 0 ]
+    printf ' ✖'
+  end
+end
+
+function prompt::lambda
+  set_color -b normal magenta
+  printf 'λ '
+end
+
+function fish_prompt
+  set -l last_status $status
+  printf '\n'
+  prompt::dir
+  # prompt::git
+  prompt::status $last_status
+  printf '\n'
+  prompt::lambda
+
+  set_color -b normal normal
+end
