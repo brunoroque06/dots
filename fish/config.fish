fd614883365a2853827161e8daea21599a461869
Bruno Roque <bruno.roque@spoud.io>
2019-04-03 23:15:06 +0200

    Remove function to symlink

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,9 @@
-# Colors
+# Commands
 set -U fish_color_command green
 set -U fish_color_param normal
 set -U fish_color_quote yellow
+
+# fish_vi_key_bindings
+fish_default_key_bindings
+
+# set PATH /usr/local/bin /usr/sbin $PATH
