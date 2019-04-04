c80dd1216fbd673bca46554e98da16b99693bcae
Bruno Roque <bruno.roque@spoud.io>
2019-04-04 23:15:42 +0200

    Migrate zsh aliases to fish

--- fish/abbrs.fish
+++ fish/abbrs.fish
@@ -1,3 +1,14 @@
+# Arch Linux
+abbr s startx
+
+# Bazel
+abbr bb 'bazel build --verbose_failures'
+abbr bba 'bazel build --verbose_failures //...'
+abbr br 'bazel run'
+abbr bt 'bazel test'
+abbr btcn 'bazel test --cache_test_results=no'
+abbr bta 'bazel test //...'
+
 # Brew
 abbr brew_update 'brew update; brew upgrade; brew cleanup; brew doctor'
 
@@ -55,3 +66,50 @@ abbr got 'go test ./...'
 # Java
 abbr jr 'java -jar'
 abbr jv 'jar tvf'
+
+# Makefile
+abbr m 'make'
+
+# Mundane Life
+abbr cal3 'cal -3'
+abbr caly 'cal -y'
+abbr cv_update 'cp "$HOME"/Downloads/curriculum_vitae.pdf "$HOME"/Projects/brunoroque06.github.io/documents/BrunoRoqueCv.pdf; tar -xf "$HOME"/Downloads/curriculum-vitae.zip -C "$HOME"/Projects/brunoroque06.github.io/documents/curriculum-vitae'
+
+# Npm
+abbr nci 'npm ci'
+abbr ni 'npm install'
+abbr npm_global_list 'npm list -g --depth=0'
+abbr npm_global_update 'npm update -g'
+abbr nr 'npm run'
+abbr ns "awk '/\"scripts\":/,/}/' package.json"
+
+# Pacman
+abbr pacman_update 'sudo pacman -Syu'
+abbr yay_update 'yay -Syu'
+
+# Processes
+abbr list_ports 'lsof -PiTCP G LISTEN'
+abbr k9 'kill -9'
+
+# Shell
+abbr shell_scripts_lint 'find . -not -path "./.git/*" -type f \( ! -name "*.*" -o -name ".*" \) -exec shellcheck -x -e SC1090 {} \;'
+abbr shell_scripts_format 'shfmt -i 2 -s -w .'
+
+# Tmux
+abbr ta 'tmux attach-session -t'
+abbr tks 'tmux kill-server'
+abbr tl 'tmux list-sessions'
+abbr tn 'tmux new-session -s default'
+
+# Vim
+abbr v 'vim'
+abbr vim_none 'vim -u NONE'
+abbr vim_update 'vim +PlugUpgrade +PlugUpdate! +PlugClean! +qa'
+
+# VSCode
+abbr code_extensions_export 'code --list-extensions > $HOME/.vscode/extensions.txt'
+abbr code_extensions_install '< $HOME/.vscode/extensions.txt xargs -L 1 code --install-extension'
+
+# Yank/Paste
+abbr p 'pbpaste'
+abbr y 'pbcopy'
