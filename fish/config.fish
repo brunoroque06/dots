8dca8175ebf0011f1f630c0dbebfe25afb21c5d1
Bruno Roque <brunoroque06@gmail.com>
2019-07-10 18:27:43 +0200

    Remove fish_prompt, and add pure

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,20 @@
 # Vim
 fish_vi_key_bindings
 
-# Theme
-set -U fish_color_command green
-set -U fish_color_param normal
-set -U fish_color_quote yellow
-
 # Binds
 bind -M insert \ce accept-autosuggestion
 bind -M insert \cn history-search-forward
 bind -M insert \cp history-search-backward
 
+# Commands
+set -U fish_color_command green
+set -U fish_color_param normal
+set -U fish_color_quote yellow
+
 # set PATH /usr/local/bin /usr/sbin $PATH
+
+# Prompt
+set -U pure_symbol_prompt \$
+set -U pure_symbol_git_unpulled_commits ▼
+set -U pure_symbol_git_unpushed_commits ▲
+set -U pure_reverse_prompt_symbol_in_vimode false
