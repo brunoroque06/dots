5f5b3571794467ac83cd5a588a9c25780c77658e
Bruno Roque <brunoroque06@gmail.com>
2019-09-08 22:08:27 +0200

    Run hooks

--- utilities/command.sh
+++ utilities/command.sh
@@ -3,9 +3,9 @@
 does_command_not_exist() { ! [ -x "$(command -v "$1")" ]; }
 
 prompt_for_command() {
-  read -p "Running command: $1. Are you sure? (Yy) " -n 1 -r
-  printf '\n'
-  if [[ $REPLY =~ Y|y ]]; then
-    $1
-  fi
+	read -p "Running command: $1. Are you sure? (Yy) " -n 1 -r
+	printf '\n'
+	if [[ $REPLY =~ Y|y ]]; then
+		$1
+	fi
 }
