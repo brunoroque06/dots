ba97beb5c0628a0eb88badc90a3824bab60344a4
Bruno Roque <brunoroque06@gmail.com>
2019-11-11 14:50:10 +0100

    Add readonly to bash scripts variables

--- utility/file.sh
+++ utility/file.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 symlink_to_dir() {
-	FILE=$1
-	TARGET_DIR=$2
-	AB_PATH="$(
+	readonly FILE=$1
+	readonly TARGET_DIR=$2
+	readonly AB_PATH="$(
 		cd "$(dirname "$0")" || exit
 		pwd -P
 	)"
 	mkdir -p "$TARGET_DIR"
-	FILE_NAME=$(basename "$FILE")
+	readonly FILE_NAME=$(basename "$FILE")
 	ln -fsv "$AB_PATH"/"$FILE" "$TARGET_DIR"/"$FILE_NAME"
 }
