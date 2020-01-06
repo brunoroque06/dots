d096675ce56128f2351bd4babb9eba11617e5b60
Bruno Roque <brunoroque06@gmail.com>
2020-01-06 17:11:40 +0100

    Fix utility/file.sh

--- utility/file.sh
+++ utility/file.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 symlink_to_dir() {
-	readonly FILE=$1
-	readonly TARGET_DIR=$2
-	readonly AB_PATH="$(
+	local FILE=$1
+	local TARGET_DIR=$2
+	local AB_PATH
+	AB_PATH="$(
 		cd "$(dirname "$0")" || exit
 		pwd -P
 	)"
 	mkdir -p "$TARGET_DIR"
-	readonly FILE_NAME=$(basename "$FILE")
+	local FILE_NAME
+	FILE_NAME=$(basename "$FILE")
 	ln -fsv "$AB_PATH"/"$FILE" "$TARGET_DIR"/"$FILE_NAME"
 }
