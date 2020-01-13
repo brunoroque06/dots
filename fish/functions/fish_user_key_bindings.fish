3323c0b97f2a022511d9d5949563f9532974d4d0
Bruno Roque <brunoroque06@gmail.com>
2020-01-13 20:08:55 +0100

    Remove duplicated key binding

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -4,7 +4,6 @@ function fish_user_key_bindings
   bind -M insert \cn history-search-forward
   bind -M insert \cp history-search-backward
   bind -M insert \cf fzf-file-widget
-  bind -M insert \cf fzf-file-widget
   bind -e -M insert \ct
   bind -e -M default \ct
 end
