1a5c23edaeafcbb67500c101978733f5e27879f7
Bruno Roque <brunoroque06@gmail.com>
2020-01-13 20:06:35 +0100

    Fix open_url

--- fish/functions/open_url.fish
+++ fish/functions/open_url.fish
@@ -1,7 +1,7 @@
 function open_url
   set -l IFS
   set -l screen (tmux capture-pane -J -p)
-  set -l urls (printf "$screen" | grep -oE '\\w+(?:\\.\\w+)+' | sed 's/[^ ]* */https:\/\/&/g' | tr ' ' '\n' | sort)
+  set -l urls (printf "$screen" | grep -oE '[[:alnum:]-]+(?:[:\.][[:alnum:]-]+)+' | sed 's/[^ ]* */https:\/\/&/g' | tr ' ' '\n' | sort --unique)
   if test -z "$urls"
     printf "No identitied urls"
   else
