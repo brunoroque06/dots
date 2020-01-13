e13ecbaa79419d30d3c112bfa05987cbdbb97de3
Bruno Roque <brunoroque06@gmail.com>
2020-01-13 23:30:09 +0100

    Remove % from open_url (grep does not like it)

--- fish/functions/open_url.fish
+++ fish/functions/open_url.fish
@@ -2,7 +2,7 @@ function open_url
   set -l panes (tmux list-panes -s -F '#{pane_id}')
   set -l content
   for pane in $panes
-    set -a content (tmux capture-pane -J -p -t $pane) 
+    set -a content (tmux capture-pane -J -p -t $pane | tr '%' ' ') 
   end
   set -l IFS
   set -l urls (printf "$content" | grep -oE '[[:alnum:]-]+(?:[:\.][[:alnum:]-]+)+' | sort --unique | sed 's/^/https:\/\//' | tr ' ' '\n')
