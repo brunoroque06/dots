c1518cdedb8445375e42db579284acb023a5ccd0
Bruno Roque <brunoroque06@gmail.com>
2020-02-04 22:36:28 +0100

    Replace ctrl+r by ctrl+c to run command in VSCode and Fish shell

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,14 @@
 function fish_user_key_bindings
   fzf_key_bindings
+
+  bind -M insert \cc fzf-history-widget
   bind -M insert \ce accept-autosuggestion
+  bind -M insert \cf fzf-file-widget
   bind -M insert \cn history-search-forward
   bind -M insert \cp history-search-backward
-  bind -M insert \cf fzf-file-widget
-  bind -e -M insert \ct
+
+  bind -e -M default \cr
+  bind -e -M insert \cr
   bind -e -M default \ct
+  bind -e -M insert \ct
 end
