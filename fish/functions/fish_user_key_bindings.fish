61636c2446102e029a43866ff85ac68d7e8e9a93
Bruno Roque <brunoroque06@gmail.com>
2020-02-05 11:39:40 +0100

    Revert fish cc, as it was a conflict with the kill command. The default cr is used instead

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,14 +1,11 @@
 function fish_user_key_bindings
   fzf_key_bindings
 
-  bind -M insert \cc fzf-history-widget
   bind -M insert \ce accept-autosuggestion
   bind -M insert \cf fzf-file-widget
   bind -M insert \cn history-search-forward
   bind -M insert \cp history-search-backward
 
-  bind -e -M default \cr
-  bind -e -M insert \cr
   bind -e -M default \ct
   bind -e -M insert \ct
 end
