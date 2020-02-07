4ec8f6152347168358796d5d07ab3373e381a02c
Bruno Roque <brunoroque06@gmail.com>
2020-02-07 20:27:58 +0100

    Add bind to delete word in fish shell

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,14 +1,14 @@
 function fish_user_key_bindings
   fzf_key_bindings
 
+  bind -M insert -k dc delete-char
   bind -M insert \ce accept-autosuggestion
   bind -M insert \cf fzf-file-widget
   bind -M insert \cn history-search-forward
   bind -M insert \cp history-search-backward
-
-  bind -e -M default \ct
+  bind -M insert \e\x7f backward-kill-word
   bind -e -M insert \ct
 
   bind -M default -k dc forward-char
-  bind -M insert -k dc delete-char
+  bind -e -M default \ct
 end
