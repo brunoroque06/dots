b97e2ff04330a41f917a6ee081ed33e9879cc0ea
Bruno Roque <brunoroque06@gmail.com>
2020-02-14 19:20:33 +0100

    Change keybinds

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,10 +3,10 @@ function fish_user_key_bindings
 
   bind -M insert -k dc delete-char
   bind -M insert \ce accept-autosuggestion
-  bind -M insert \cf fzf-file-widget
   bind -M insert \cn history-search-forward
+  bind -M insert \co fzf-file-widget
   bind -M insert \cp history-search-backward
-  bind -M insert \e\x7f backward-kill-word
+  bind -M insert \cw backward-kill-word
   bind -e -M insert \ct
 
   bind -M default -k dc forward-char
