cf76337833eaff3e2e32a573e48273b17e3e3c71
Bruno Roque <brunoroque06@gmail.com>
2020-05-29 00:18:01 +0200

    Rename open_url to zu

--- fish/functions/zu.fish
+++ fish/functions/zu.fish
@@ -1,8 +1,8 @@
-function open_url
+function zu
   set -l panes (tmux list-panes -s -F '#{pane_id}')
   set -l content
   for pane in $panes
-    set -a content (tmux capture-pane -J -p -t $pane | tr '%' ' ') 
+    set -a content (tmux capture-pane -J -p -t $pane | tr '%' ' ')
   end
   set -l IFS
   set -l urls (printf "$content" | grep -oE '[[:alnum:]-]+(?:[:\.][[:alnum:]-]+)+(?:/[[:alnum:]-]+)*' | sort --unique | sed 's/^/http:\/\//' | tr ' ' '\n')
