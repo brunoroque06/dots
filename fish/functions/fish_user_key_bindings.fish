cf0bd913bf14eb85ce618f844dd29c6b3690d91b
Bruno Roque <brunoroque06@gmail.com>
2021-03-04 19:56:04 +0100

    Use leader + r to everything run related (run/debug/stop)

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,18 +3,13 @@ function fish_user_key_bindings
 
   bind -M insert -k dc delete-char
   bind -M insert \ca fzf-history-widget
-  bind -M insert \ce accept-autosuggestion
-  bind -M insert \cd fzf-cd-widget
-  bind -M insert \cn history-search-forward
-  bind -M insert \co fzf-file-widget
-  bind -M insert \cp history-search-backward
+  bind -M insert \cf fzf-file-widget
+  bind -M insert \cj history-search-forward
+  bind -M insert \ck history-search-backward
+  bind -M insert \cl accept-autosuggestion
   bind -M insert \cw backward-kill-word
   bind -M insert \e\x7F backward-kill-word
-  bind -e -M insert \ct
+  bind -M insert \cx fzf-cd-widget
 
   bind -M default -k dc forward-char
-  bind -e -M default \ct
-
-  bind -e --preset -M insert \cd
-  bind -e --preset -M visual \cd
 end
