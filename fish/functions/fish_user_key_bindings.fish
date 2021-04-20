bb65164f398d310ba48ccb8ff8ef35daee954405
Bruno Roque <brunoroque06@gmail.com>
2021-04-20 19:54:00 +0200

    Clean unused fish binds

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -4,10 +4,6 @@ function fish_user_key_bindings
   bind -M insert -k dc delete-char
   bind -M insert \ca fzf-history-widget
   bind -M insert \cf fzf-file-widget
-  bind -M insert \cj history-search-forward
-  bind -M insert \ck history-search-backward
-  bind -M insert \cl accept-autosuggestion
-  bind -M insert \cw backward-kill-word
   bind -M insert \e\x7F backward-kill-word
   bind -M insert \cx fzf-cd-widget
 
