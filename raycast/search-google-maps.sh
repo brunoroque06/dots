690bcd25ba9855a384a5f1531502256e40e0e513
Bruno Roque <brunoroque06@gmail.com>
2022-03-16 23:25:46 +0100

    Replace safari link extension and add keyword search to raycast

---
+++ raycast/search-google-maps.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Search Google Maps
+# @raycast.mode silent
+# @raycast.icon 🔍
+# @raycast.argument1 { "type": "text", "placeholder": "keyword(s)", "percentEncoded": true }
+
+open https://www.google.com/maps?oi=map\&q=$1
---
+++ raycast/search-youtube.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Search YouTube
+# @raycast.mode silent
+# @raycast.icon 🔍
+# @raycast.argument1 { "type": "text", "placeholder": "keyword(s)", "percentEncoded": true }
+
+open https://www.youtube.com/results?search_query=$1
