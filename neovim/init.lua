80787efbc30269ec4baa76ded78be4fe7f3d10ae
Bruno Roque <brunoroque06@gmail.com>
2022-04-26 21:48:17 +0200

    Vscode does not format on save

--- neovim/init.lua
+++ neovim/init.lua
@@ -375,7 +375,6 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gh", vim.lsp.buf.hover)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
 vim.keymap.set("n", "gr", vim.lsp.buf.rename)
-vim.keymap.set("n", "gs", ":Telescope treesitter<cr>")
 vim.keymap.set("n", "gu", ":Telescope lsp_references<cr>")
 
 vim.keymap.set("n", "<leader>w", ":w<cr>")
